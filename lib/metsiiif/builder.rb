ff1dd7d94d1edcd36945ab82f9063a0b499f553c
Adam Jazairi <adamjazairi@gmail.com>
2018-01-31 13:19:53 -0500

    Add range building method

--- lib/metsiiif/builder.rb
+++ lib/metsiiif/builder.rb
@@ -1,7 +1,6 @@
 require 'iiif/presentation'
 require 'metsiiif/mets_file'
 
-
 module Metsiiif
 
   class Builder
@@ -20,8 +19,12 @@ module Metsiiif
       sequence = IIIF::Presentation::Sequence.new
       sequence.canvases = mets_file.struct_map.map.with_index {|comp, i| image_annotation_from_id("#{comp}.jp2", "#{comp}", i)}
 
+      range = IIIF::Presentation::Range.new
+      range.ranges = mets_file.struct_map.map.with_index {|comp, i| build_range("#{@sequence_base}/canvases/#{page_id}", "#{comp}", i)}
+
       manifest = build_manifest(mets_file)
       manifest.sequences << sequence
+      manifest.structures << range
       thumb = sequence.canvases.first.images.first.resource['@id']
       manifest.insert_after(existing_key: 'label', new_key: 'thumbnail', value: thumb)
 
@@ -43,7 +46,7 @@ module Metsiiif
       image_id = image_file.chomp('.jp2').chomp('.tif').chomp('.tiff')
       page_id = image_id.split(separator).last
 
-      canvas_id = "#{@sequence_base}/#{page_id}"
+      canvas_id = "#{@sequence_base}/canvases/#{page_id}"
 
       seed = {
           '@id' => "#{canvas_id}/annotations/1",
@@ -61,11 +64,21 @@ module Metsiiif
           'label' => label,
           'width' => annotation.resource['width'],
           'height' => annotation.resource['height'],
-          'images' => [annotation],
+          'images' => [annotation]
       }
       IIIF::Presentation::Canvas.new(seed)
     end
 
+    def build_range(canvas_id, label, order)
+      range_id = "#{@sequence_base}/ranges/r-#{order}"
+
+      seed = {
+          '@id' => range_id,
+          'label' => label,
+          'canvases' => "[#{canvas_id}]"
+      }
+      IIIF::Presentation::Range.new(seed)
+    end
 
     def image_resource_from_page_hash(page_id)
       base_uri = "#{@iiif_host}/#{page_id}"
