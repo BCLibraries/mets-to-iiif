efffdf9e80563957e6941195b9cf569252981146
Adam Jazairi <adamjazairi@gmail.com>
2018-02-14 14:39:11 -0500

    Handle local variations to mods:accessCondition

--- lib/metsiiif/mods_record.rb
+++ lib/metsiiif/mods_record.rb
@@ -3,7 +3,11 @@ module Metsiiif
 
     TITLE_TEXT = 'mods:titleInfo[@usage="primary"]/mods:title/text()'
     RELATED_ITEM = 'mods:relatedItem[@type="host"]'
-    ACCESS_CONDITION = 'mods:accessCondition[@type="useAndReproduction"][@displayLabel="Conditions Governing Use note"]/text()'
+
+    # Access condition constants
+    AC_USE_AND_REPRODUCTION = 'mods:accessCondition[@type="useAndReproduction"]/text()'
+    AC_RESTRICTION_ON_ACCESS = 'mods:accessCondition[@type="restrictionOnAccess"]/text()'
+    AC_DISPLAY_LABEL = 'mods:accessCondition[@type="useAndReproduction"][@displayLabel="Conditions Governing Use note"]/text()'
 
     # @param [Nokogiri::XML::Node] mods_record
     def initialize(mods_record)
@@ -19,7 +23,13 @@ module Metsiiif
     end
 
     def rights_information
-      @mods_record.xpath("#{ACCESS_CONDITION}", 'mods' => 'http://www.loc.gov/mods/v3').to_s
+      if AC_USE_AND_REPRODUCTION.length > 0
+        @mods_record.xpath("#{AC_USE_AND_REPRODUCTION}", 'mods' => 'http://www.loc.gov/mods/v3').to_s
+      elsif AC_RESTRICTION_ON_ACCESS.length > 0
+        @mods_record.xpath("#{AC_RESTRICTION_ON_ACCESS}", 'mods' => 'http://www.loc.gov/mods/v3').to_s
+      elsif AC_DISPLAY_LABEL.length > 0
+        @mods_record.xpath("#{AC_DISPLAY_LABEL}", 'mods' => 'http://www.loc.gov/mods/v3').to_s
+      end
     end
   end
 end
