d5b1c5220d139a6557b98a7087837c50db4e4e8e
Adam Jazairi <adamjazairi@gmail.com>
2018-02-27 13:30:25 -0500

    Reverse labels

--- lib/metsiiif/builder.rb
+++ lib/metsiiif/builder.rb
@@ -37,7 +37,7 @@ module Metsiiif
     def build_manifest(mets_file)
       seed = {
           '@id' => "#{@manifest_host}/#{mets_file.obj_id}.json",
-          'label' => "#{mets_file.mods.host_title} #{mets_file.mods.title}",
+          'label' => "#{mets_file.mods.title}, #{mets_file.mods.host_title}",
           'viewing_hint' => 'paged',
           'description' => "#{mets_file.mods.abstract}",
           'attribution' => "#{mets_file.mods.rights_information}",
