68a8b79073d7da4d61d029f9481ca1df383a980d
Adam Jazairi <adamjazairi@gmail.com>
2018-03-27 11:32:55 -0400

    Add collection_identifier and owner methods

--- lib/metsiiif/mods_record.rb
+++ lib/metsiiif/mods_record.rb
@@ -4,6 +4,9 @@ module Metsiiif
     TITLE_TEXT = 'mods:titleInfo[@usage="primary"]/mods:title/text()'
     RELATED_ITEM = 'mods:relatedItem[@type="host"]'
     ABSTRACT = 'mods:mods/mods:abstract'
+    ACCESSION_NUMBER = 'mods:identifier[@type="accession number"]'
+    DISPLAYFORM = 'mods:name/mods:displayForm'
+    ROLETERM = 'mods:name/mods:role/mods:roleTerm[@type="text"]'
 
     # Access condition constants
     AC_USE_AND_REPRODUCTION = 'mods:accessCondition[@type="useAndReproduction"]/text()'
@@ -30,6 +33,16 @@ module Metsiiif
         @mods_record.xpath("#{AC_RESTRICTION_ON_ACCESS}", 'mods' => 'http://www.loc.gov/mods/v3').to_s
       elsif AC_DISPLAY_LABEL.length > 0
         @mods_record.xpath("#{AC_DISPLAY_LABEL}", 'mods' => 'http://www.loc.gov/mods/v3').to_s
+      end
+    end
+
+    def collection_identifier
+      @mods_record.xpath("#{RELATED_ITEM}/#{ACCESSION_NUMBER}", 'mods' => 'http://www.loc.gov/mods/v3').to_s
+    end
+
+    def owner
+      if ROLETERM == 'Owner'
+        @mods_record.xpath("#{DISPLAYFORM}", 'mods' => 'http://www.loc.gov/mods/v3').to_s
       end
     end
   end
