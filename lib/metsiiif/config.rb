50be2f33b15617a92edae7f8e27fe11d5c75ec3e
Adam Jazairi <adamjazairi@gmail.com>
2018-04-26 10:39:56 -0400

    Fix ref to config.yml

--- lib/metsiiif/config.rb
+++ lib/metsiiif/config.rb
@@ -3,7 +3,7 @@ require 'yaml'
 module Metsiiif
   class Config
     attr_reader :cnf
-    @cnf = YAML::load_file(File.join(__dir__, '/config.yml'))
+    @cnf = YAML::load_file(File.join(__dir__, '../../config.yml'))
     def self.cnf
       @cnf
     end
