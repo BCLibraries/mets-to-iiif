5d5404a3310f3048a5abdb39f7ff9522ac18e9ef
Adam Jazairi <adamjazairi@gmail.com>
2018-05-07 15:44:29 -0400

    Fix excessive whitespace in preferred citation

--- lib/metsiiif/builder.rb
+++ lib/metsiiif/builder.rb
@@ -48,10 +48,7 @@ module Metsiiif
           'attribution' => "#{mets_file.mods.rights_information}",
           'metadata' => [
             {"handle": "#{mets_file.handle}"},
-            {"label": "Preferred Citation", "value": "#{mets_file.mods.creator + 
-              ", " unless mets_file.mods.creator.nil?}
-              #{mods_title}#{": " + mets_file.mods.subtitle unless mets_file.mods.subtitle.nil?}, 
-              #{mets_file.sequence_label}, #{mets_file.mods.owner}, #{mets_file.handle}."}
+            {"label": "Preferred Citation", "value": "#{mets_file.mods.creator + ", " unless mets_file.mods.creator.nil?}#{mods_title}#{": " + mets_file.mods.subtitle unless mets_file.mods.subtitle.nil?}, #{mets_file.sequence_label}, #{mets_file.mods.owner}, #{mets_file.handle}."}
           ]
       }
       IIIF::Presentation::Manifest.new(seed)
