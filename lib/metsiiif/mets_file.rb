f43b478df3aaab3024b9a84d07040f162350561a
Adam Jazairi <adamjazairi@gmail.com>
2018-05-11 09:35:30 -0400

    Add conditional obj_id for Dutch book of hours

--- lib/metsiiif/mets_file.rb
+++ lib/metsiiif/mets_file.rb
@@ -20,6 +20,8 @@ module Metsiiif
       elsif mods.localcollection.include?('bcimage')
         prefix = "bcimage_"
         prefix + uri.split('/').last
+      elsif mods.title.include?('MS.1986.093')
+        "MS1986_093"
       else
         uri.split('/').last
       end
