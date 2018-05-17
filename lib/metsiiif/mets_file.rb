62e4221b528a8199e6f1cfbee6ab489be0398d2a
Adam Jazairi <adamjazairi@gmail.com>
2018-05-17 16:17:21 -0400

    Fix component_label method

--- lib/metsiiif/mets_file.rb
+++ lib/metsiiif/mets_file.rb
@@ -64,7 +64,7 @@ module Metsiiif
     end
 
     def component_label
-      @doc.xpath("#{@structmap}/#{@component_div}/@LABEL", 'mets' => 'http://www.loc.gov/METS/').to_s.strip
+      @doc.xpath("#{@structmap}/#{@sequence_div}/#{@component_div}/@LABEL", 'mets' => 'http://www.loc.gov/METS/').to_s.strip
     end
   end
 end
