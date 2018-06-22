5961c8b152155fbddaaaaa8d7656e98831570638
Adam Jazairi <adamjazairi@gmail.com>
2018-06-22 09:49:39 -0400

    Conditional obj_id for Hayward and Roche

--- lib/metsiiif/mets_file.rb
+++ lib/metsiiif/mets_file.rb
@@ -20,7 +20,7 @@ module Metsiiif
       elsif mods.localcollection.any? { |coll| coll.include?('bcimage') }
         prefix = "bcimage_"
         prefix + uri.split('/').last
-      elsif mods.host_title.length == 0 || mods.host_title.include?("Leary")
+      elsif mods.host_title.length == 0 || mods.host_title.include?("Leary") || mods.host_title.include?("Hayward") || mods.host_title.include?("Roche")
         mods.identifier
       else
         uri.split('/').last
